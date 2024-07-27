d241ea85ea6de220ecd9462411c200dd169dfa39
Spone <spone185@gmail.com>
2024-07-27 10:51:02 +0300

    Initialize project, add player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+
+    [SerializeField] private float movingSpeed = 5f;
+
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Vector2 inputVector = GameInput.Instance.GetMovementVector();
+        rb.MovePosition(rb.position + inputVector * (movingSpeed * Time.fixedDeltaTime));
+    }
+}
