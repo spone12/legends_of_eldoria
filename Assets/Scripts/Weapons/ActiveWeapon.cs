ca209329f878d078897c7fa733fbe23f308b2c7e
Spone <spone185@gmail.com>
2024-11-30 10:04:18 +0300

    Add sword slash visual

--- Assets/Scripts/Weapons/ActiveWeapon.cs
+++ Assets/Scripts/Weapons/ActiveWeapon.cs
@@ -11,7 +11,22 @@ public class ActiveWeapon : MonoBehaviour
         Instance = this;
     }
 
+    private void Update() {
+        FollowMousePosition();
+    }
+
     public Sword GetActiveWeapon() {
         return sword;
+    }
+
+    private void FollowMousePosition() {
+        Vector3 mousePos = GameInput.Instance.GetMousePosition();
+        Vector3 playerPos = Player.Instanse.GetPlayerScreenPostion();
+
+        if (mousePos.x < playerPos.x) {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        } else {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 }
---
+++ Assets/Scripts/Weapons/Sword/SlwordSlashVisual.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SwordSlashVisual : MonoBehaviour
+{
+    private Animator animator;
+
+    [SerializeField] private Sword sword;
+
+    private const string ATTACK = "Attack";
+
+    private void Awake() {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start() {
+        sword.OnSwordSwing += Sword_OnSwordSwing;
+    }
+
+    private void Sword_OnSwordSwing(object sender, System.EventArgs e) {
+        animator.SetTrigger(ATTACK);
+    }
+}
