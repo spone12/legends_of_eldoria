235d0cdb8846d185df85a1ff14b1a5d014d6843a
Spone <spone185@gmail.com>
2024-12-08 12:26:10 +0300

    Added a health bar

--- Assets/Scripts/Characters/Player/PlayerHealth.cs
+++ Assets/Scripts/Characters/Player/PlayerHealth.cs
@@ -1,27 +1,36 @@
 using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
-using static UnityEditor.ShaderGraph.Internal.KeywordDependentCollection;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public static PlayerHealth Instanse { get; private set; }
 
+    [Header("Health")]
+    
     [SerializeField] private int _maxHealth = 30;
+    [SerializeField] private int _currentHealth;
 
-    private KnockBack _knockBack;
     public event EventHandler OnPlayerDeath;
 
-    private int _currentHealth;
+    private KnockBack _knockBack;
+    private Slider _hearthSlider;
+    private TMP_Text _hpValueText;
+
     private bool _canTakeDamage = true;
 
     private void Awake() {
         Instanse = this;
+        _hearthSlider = GameObject.Find("HealthPointsSlider").GetComponent<Slider>();
+        _hpValueText = GameObject.Find("HPBarValue").GetComponent<TMP_Text>();
         _knockBack = GetComponent<KnockBack>();
     }
 
     private void Start() {
         _currentHealth = _maxHealth;
+        UpdateHealthSlider();
     }
 
     /**
@@ -30,11 +39,12 @@ public class PlayerHealth : MonoBehaviour
     public void TakeDamage(Transform damageSource, int damage, float damageRecoveryTime = 0.5f) {
 
         if (Player.Instanse.IsPlayerAlive() && _canTakeDamage) {
+
             _canTakeDamage = false;
             _currentHealth = Mathf.Max(0, _currentHealth -= damage);
-            Debug.Log(_currentHealth);
-            _knockBack.GetKnockedBack(damageSource);
+            UpdateHealthSlider();
 
+            _knockBack.GetKnockedBack(damageSource);
             StartCoroutine(DamageRecoveryCorutine(damageRecoveryTime));
         }
 
@@ -53,6 +63,15 @@ public class PlayerHealth : MonoBehaviour
 
             OnPlayerDeath?.Invoke(this, EventArgs.Empty);
         }
+    }
+
+    /**
+     * Health slider update
+     */
+    private void UpdateHealthSlider() {
+        _hearthSlider.maxValue = _maxHealth;
+        _hearthSlider.value = _currentHealth;
+        _hpValueText.text = $"{_currentHealth} / {_maxHealth}";
     }
 
     /**
